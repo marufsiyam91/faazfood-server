cbd79e3a5c9b353641186815e86a4bd0c6263de7
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-11 22:09:17 +0600

    some update

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -1,122 +1,123 @@
-const Food = require('../models/foodModels')
-
-
+const Food = require('../models/foodModels');
+const ApiFeatures = require('../utills/apiFeatures')
 
 const getAllFoodItems = async (req, res) => {
     try {
-//1) filtering
-        const queryObj = { ...req.query }
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach(el => delete queryObj[el])
-//2) advanced filtering
-        let querySrt = JSON.stringify(queryObj);
-        querySrt = querySrt.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(querySrt))
-
-        let query = Food.find(JSON.parse(querySrt))
-//3) sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy)
-        }
-//4) pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 12;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit)
-
-        if (req.query.page) {
-            const numFoods = await Food.countDocuments();
-            if(skip >= numFoods) throw new Error('This page does not exist')
-        }
+        const features = new ApiFeatures(Food.find(), req.query)
+            .filter()
+            .sort()
+            .paginate();
         
-        let allFoodItems = await query
+        const allFoodItems = await features.query;
 
-        console.log(queryObj)
-    
         res.json({
             status: 'success',
-            result: allFoodItems.length,
-            data: allFoodItems
-        })
+            results: allFoodItems.length,
+            data: allFoodItems,
+        });
     } catch (err) {
         res.status(404).json({
             status: 'fail',
-            message: err
-        })
+            message: err.message,
+        });
     }
-}
+};
 
 const createFoodItem = async (req, res) => {
     try {
-        const newFood = await Food.create(req.body)
+        const newFood = await Food.create(req.body);
         
-        res.status(200).json({
+        res.status(201).json({
             status: 'success',
             data: {
-                food: newFood
-            }
-        })
-    } catch (error) {
-        res.status(404).json({
+                food: newFood,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
             status: 'fail',
-            message: error
-        })
+            message: err.message,
+        });
     }
-
-}
+};
 
 const getFoodItem = async (req, res) => {
     try {
-        const foodItem = await Food.findById(req.params.id)
+        const foodItem = await Food.findById(req.params.id);
+
+        if (!foodItem) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'No food item found with that ID',
+            });
+        }
 
         res.status(200).json({
             status: 'success',
-            data: {
-                data: foodItem
-            }
-        })
+            data: foodItem,
+        });
     } catch (err) {
         res.status(404).json({
             status: 'fail',
-            message: err
-        })
+            message: err.message,
+        });
     }
-}
+};
 
-const updateFoodItem = async (req, res) => {    
+const updateFoodItem = async (req, res) => {
     try {
         const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
-            runValidators: true
-        })
+            runValidators: true,
+        });
+
+        if (!food) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'No food item found with that ID',
+            });
+        }
+
         res.status(200).json({
             status: 'success',
-            message: 'food updated successfully'
-        })
+            message: 'Food item updated successfully',
+            data: food,
+        });
     } catch (err) {
-        res.status(404).json({
+        res.status(400).json({
             status: 'fail',
-            message: err
-        })
+            message: err.message,
+        });
     }
-}
+};
 
 const deleteFoodItem = async (req, res) => {
     try {
-        await Food.findByIdAndDelete(req.params.id )
+        const food = await Food.findByIdAndDelete(req.params.id);
+
+        if (!food) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'No food item found with that ID',
+            });
+        }
 
         res.status(204).json({
             status: 'success',
-            message: 'foodItem deleted successfully'
-        })
+            message: 'Food item deleted successfully',
+        });
     } catch (err) {
         res.status(404).json({
             status: 'fail',
-            message: err
-        })
+            message: err.message,
+        });
     }
-}
+};
 
-module.exports = {getAllFoodItems, createFoodItem, getFoodItem, updateFoodItem, deleteFoodItem}
+module.exports = {
+    getAllFoodItems,
+    createFoodItem,
+    getFoodItem,
+    updateFoodItem,
+    deleteFoodItem,
+};
